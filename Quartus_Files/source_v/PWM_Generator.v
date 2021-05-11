a1b2a6f2febd809e7294184abbec5d71df68d60b
Franz <fk.luepke146@uniandes.edu.co>
2021-05-10 22:52:32 -0500

    Syntax errors fix.
    - RPM: Signal bits length from 36 to 32.
    - PWM: Comments changed.
    - Encoder: Spaces align. (Aesthetic changes)

--- Quartus_Files/source_v/PWM_Generator.v
+++ Quartus_Files/source_v/PWM_Generator.v
@@ -1,19 +1,20 @@
 // Generates PWM signal from an 8 bits number, betwenn 0 and 249.
 module PWM_Generator(clock, reset, PWM_in, PWM_out);
 	// INPUTS AND OUTPUTS
-	input 			clock;		// Clock
-	input 			reset;		// Reset
-	input	[7:0]	PWM_in;		// PWM requested
-	output 			PWM_out;	// PWM out signal
+	input 			clock;					// Clock
+	input 			reset;					// Reset
+	input	[7:0]		PWM_in;					// PWM requested
+	output 			PWM_out;					// PWM out signal
 	// REGS
-	reg				actual_output	= 0;				// Salida
-	assign			PWM_out			= actual_output;	// Asignacion de la salida
+	reg				actual_output	= 0;	// Present output
 	// WIRES
-	wire 			posFlag;
-	wire 			negFlag;
-	// MODULOS
-	Cycles_Counter posCounter(clock, reset, 2500, posFlag);			// Indicates when to rise the output.
-	Cycles_Counter negCounter(clock, posFlag, 10*PWM_in, negFlag);	// Indicates when to lower the output.
+	wire 				posFlag;
+	wire 				negFlag;
+	// ASSIGNS
+	assign			PWM_out			= actual_output;	// Output assign
+	// MODULES
+	Cycles_Counter posCounter(clock, reset,	2500,			posFlag);	// Indicates when to rise the output.
+	Cycles_Counter negCounter(clock, posFlag, 10*PWM_in,	negFlag);	// Indicates when to lower the output.
 	// The descent depends on the input, because it modulates the pulse width.
 	// Sequential logic. STATE MACHINE
 	always@(posedge clock) // State machine that translate flags into the signal.
