08ce603ef9f478fa43bd8de16f95d5c671a7a162
Franz <fk.luepke146@uniandes.edu.co>
2021-07-09 08:58:47 -0500

    Avalon RPM fix.

--- software/kernelspace/avalon_rpm/avalon_rpm.c
+++ software/kernelspace/avalon_rpm/avalon_rpm.c
@@ -1,217 +1,261 @@
+// Includes
 #include <linux/module.h>
 #include <linux/platform_device.h>
-#include <linux/io.h>
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
-#include <linux/types.h>
+#include <linux/io.h>
 #include <linux/uaccess.h>
+// Define information about this kernel module
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Franz Luepke <fk.luepke146@uniandes.edu.co>");
+MODULE_DESCRIPTION("Exposes a misc device to user space that lets users read the RPM value and change its parameters.");
+MODULE_VERSION("1.0");
+#define RAM_SPAN    (1024)
+#define IO_BUF_SIZE (256)
 // Variables
+unsigned long int start_address;
+unsigned long int end_address;
+// char[] name;
 static int number_devices = 0;
-static char *arr[] = { "avalon_rpm_0", "avalon_rpm_1", "avalon_rpm_2", "avalon_rpm_3", "avalon_rpm_4", "avalon_rpm_5"};
+// Drivers array
+static char *arr[] =
+{
+    "avalon_rpm_0",
+    "avalon_rpm_1",
+    "avalon_rpm_2",
+    "avalon_rpm_3",
+    "avalon_rpm_4",
+    "avalon_rpm_5"
+};
+// Globals
+char name[] = "Avalon RPM Driver";
 // Prototypes
-static int dev_open(struct inode *, struct file *);
-static int dev_release(struct inode *, struct file *);
-static int rpm_probe(struct platform_device *pdev);
-static int rpm_remove(struct platform_device *pdev);
-static ssize_t rpm_read(struct file *file, char *buffer, size_t len, loff_t *offset);
-static ssize_t rpm_write(struct file *file, const char *buffer, size_t len, loff_t *offset);
-// An instance of this structure will be created for every avalon_rpm IP in the system
-struct avalon_rpm_dev
+static ssize_t dev_open(struct inode *ip, struct file *fp);
+static int dev_release(struct inode *ip, struct file *fp);
+static int dev_probe(struct platform_device *pdev);
+static int dev_remove(struct platform_device *pdev);
+static ssize_t dev_read(struct file *fp, char __user *user_buffer, size_t len, loff_t *offset);
+static loff_t dev_llseek(struct file *fp, loff_t offset, int whence);
+static ssize_t dev_write(struct file *fp, const char __user *user_buffer, size_t len, loff_t *offset);
+// An instance of this structure will be created for every IP component in the system
+struct ip_component_dev
 {
     struct miscdevice miscdev;
     void __iomem *regs;
-    u16 rpm_value;
+    u32 value;
+    unsigned char io_buf[IO_BUF_SIZE];
 };
 // Specify which device tree devices this driver supports
-static struct of_device_id avalon_rpm_dt_ids[] =
+static struct of_device_id dev_dt_ids[] =
 {
     { .compatible = "dev,avalon-rpm" },
     { /* end of table */ }
 };
 // Inform the kernel about the devices this driver supports
-MODULE_DEVICE_TABLE(of, avalon_rpm_dt_ids);
+MODULE_DEVICE_TABLE(of, dev_dt_ids);
 // Data structure that links the probe and remove functions with our driver
-static struct platform_driver rpm_platform = {
-    .probe = rpm_probe,
-    .remove = rpm_remove,
-    .driver = {
-        .name = "Avalon PWM Driver",
+static struct platform_driver driver_platform =
+{
+    .probe = dev_probe,
+    .remove = dev_remove,
+    .driver =
+    {
+        .name = name,
         .owner = THIS_MODULE,
-        .of_match_table = avalon_rpm_dt_ids
+        .of_match_table = dev_dt_ids
     }
 };
-// The file operations that can be performed on the avalon_rpm character file
-static const struct file_operations avalon_rpm_fops =
+// The file operations that can be performed on the character file
+static const struct file_operations dev_fops =
 {
-    .open = dev_open,
     .owner = THIS_MODULE,
-    .read = rpm_read,
-    .write = rpm_write,
-    .release = dev_release
+    .open = dev_open,
+    .read = dev_read,
+    .write = dev_write,
+    .release = dev_release,
+    .llseek = dev_llseek
 };
-// Called when the driver is installed
-static int rpm_init(void)
+// Called when the driver is installed.
+static int dev_init(void)
 {
     int ret_val = 0;
-    pr_info("---------- Avalon PWM ----------\n");
-    pr_info(" rpm_init: Initializing the Avalon PWM module\n");
+    pr_info("\n---------- %s ----------\n", name);
+    pr_info("dev_init: Initializing the %s.\n", name);
     // Register our driver with the "Platform Driver" bus
-    ret_val = platform_driver_register(&rpm_platform);
+    ret_val = platform_driver_register(&driver_platform);
     if(ret_val != 0)
     {
-        pr_err(" rpm_init: platform_driver_register returned %d\n", ret_val);
+        pr_err(" dev_init: platform_driver_register returned %d\n", ret_val);
         return ret_val;
     }
-    pr_info(" rpm_init: Avalon PWM module successfully initialized!\n");
+    pr_info("dev_init: %s successfully initialized!\n", name);
+    return ret_val;
+}
+// Called when device is opened.
+static int dev_open(struct inode *ip, struct file *fp)
+{
+    pr_info(" dev_open: %s\n", "--- DEVICE OPEN ---");
     return 0;
 }
-static int dev_open(struct inode *inodep, struct file *filep)
-{
-   printk(KERN_INFO " dev_open: Device has been opened.\n");
-   return 0;
-}
 // Called whenever the kernel finds a new device that our driver can handle
-// (In our case, this should only get called for the one instantiation of the Avalon PWM module)
-static int rpm_probe(struct platform_device *pdev)
+static int dev_probe(struct platform_device *pdev)
 {
     int ret_val = -EBUSY;
-    struct avalon_rpm_dev *dev;
+    struct ip_component_dev *dev;
     struct resource *r = 0;
     number_devices += 1;
-    pr_info(" rpm_probe: enter\n");
-    // Get the memory resources for this PWM device
+    // printk(" dev_probe: %s\n", "open");
+    // Get the memory resources for this device
     r = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-    if(r == NULL)
+    if(r != NULL)
     {
-        pr_err(" rpm_probe: IORESOURCE_MEM (register space) does not exist\n");
+        start_address = (long unsigned int) r->start;
+        end_address = (long unsigned int) r->end;
+        pr_info("  r->start = 0x%08lx\n", start_address);
+        pr_info("  r->end = 0x%08lx\n", end_address);
+        pr_info("  r->name = %s\n", r->name);
+    }
+    else
+    {
+        pr_err(" dev_probe: %s\n", "IORESOURCE_MEM (register space) does not exist");
         goto bad_exit_return;
     }
     // Create structure to hold device-specific information (like the registers)
-    dev = devm_kzalloc(&pdev->dev, sizeof(struct avalon_rpm_dev), GFP_KERNEL);
+    dev = devm_kzalloc(&pdev->dev, sizeof(struct ip_component_dev), GFP_KERNEL);
     // Both request and ioremap a memory region
-    // This makes sure nobody else can grab this memory region
-    // as well as moving it into our address space so we can actually use it
     dev->regs = devm_ioremap_resource(&pdev->dev, r);
     if(IS_ERR(dev->regs))
         goto bad_ioremap;
-    // Puts PWM in 0 value (access the 0th register in the avalon PWM module)
-    dev->rpm_value = 0x00;
-    iowrite32(dev->rpm_value, dev->regs);
-    pr_info(" rpm_probe: Registering %s device.\n", arr[number_devices-1]);
+    // pr_info("  dev_probe: Registering %s device. [%d]\n", arr[number_devices-1], number_devices);
     // Initialize the misc device (this is used to create a character file in userspace)
     dev->miscdev.minor = MISC_DYNAMIC_MINOR;    // Dynamically choose a minor number
     dev->miscdev.name = arr[number_devices-1];
-    dev->miscdev.fops = &avalon_rpm_fops;
+    dev->miscdev.fops = &dev_fops;
     ret_val = misc_register(&dev->miscdev);
     if(ret_val != 0)
     {
-        pr_info(" rpm_probe: Couldn't register misc device :(");
+        pr_info(" dev_probe: Couldn't register misc device :(");
         goto bad_exit_return;
+    }
+    else
+    {
+        pr_info(" dev_probe: %s successfully registered!", arr[number_devices-1]);
     }
     // Give a pointer to the instance-specific data to the generic platform_device structure
     // so we can access this data later on (for instance, in the read and write functions)
     platform_set_drvdata(pdev, (void*)dev);
-    pr_info(" rpm_probe: exit\n");
-    return 0;
-    bad_ioremap:
-       ret_val = PTR_ERR(dev->regs);
-    bad_exit_return:
-        pr_info(" rpm_probe: bad exit :(\n");
+    // printk(" dev_probe: %s\n", "close");
     return ret_val;
+    bad_exit_return:
+        pr_info(" dev_probe: %s\n", "bad exit");
+    bad_ioremap:
+        pr_info(" dev_probe: %s\n", "bad ioremap");
+        ret_val = PTR_ERR(dev->regs);
+    return 0;
 }
-// This function gets called whenever a read operation occurs on one of the character files
-static ssize_t rpm_read(struct file *file, char *buffer, size_t len, loff_t *offset)
+// Called when reading from the device.
+static ssize_t dev_read(struct file *fp, char __user *user_buffer, size_t len, loff_t *offset)
 {
     int success = 0;
-    /* 
-    * Get the avalon_rpm_dev structure out of the miscdevice structure.
-    * Remember, the Misc subsystem has a default "open" function that will set
-    * "file"s private data to the appropriate miscdevice structure. We then use the
-    * container_of macro to get the structure that miscdevice is stored inside of (which
-    * is our avalon_rpm_dev structure that has the current rpm value).
-    */
-    struct avalon_rpm_dev *dev = container_of(file->private_data, struct avalon_rpm_dev, miscdev);
-    pr_info(" rpm_read: enter\n");
-    // Give the user the current rpm value
-    success = copy_to_user(buffer, &dev->rpm_value, sizeof(dev->rpm_value));
-    // pr_info(" rpm_read: value -> %d\n", &dev->rpm_value);
-    // If we failed to copy the value to userspace, display an error message
+    // unsigned int value = 0;
+    struct ip_component_dev *dev = container_of(fp->private_data, struct ip_component_dev, miscdev);
+    pr_info("  dev_read: open\n");
+    pr_info("   dev_read: len = %d\n", len);
+    pr_info("   dev_read: offset = %lld\n", *offset);
+    pr_info("   dev_read: len regs = %d\n", (int) sizeof(dev->regs));
+    // Get the ip_component_dev structure out of the miscdevice structure.
+    dev->value = ioread32(dev->regs + *offset);
+    pr_info("   dev_read: value = %d\n", (int) dev->value);
+    pr_info("   dev_read: %d\n", dev->miscdev.mode);
+    success = copy_to_user(user_buffer, &dev->value, sizeof(dev->value));
     if(success != 0)
     {
-        pr_info(" rpm_read: Failed to return current rpm value to userspace\n");
+        pr_info("   dev_read: Failed to return current value to userspace.\n");
         return -EFAULT; // Bad address error value. It's likely that "buffer" doesn't point to a good address
     }
-    pr_info(" rpm_read: exit\n");
-    return 0; // "0" indicates End of File, aka, it tells the user process to stop reading
+    pr_info("  dev_read: exit\n");
+    return 0;
 }
-
-// This function gets called whenever a write operation occurs on one of the character files
-static ssize_t rpm_write(struct file *file, const char *buffer, size_t len, loff_t *offset)
+// Called when requiring an offset
+static loff_t dev_llseek(struct file *fp, loff_t offset, int whence)
+{
+    // struct ip_component_dev *dev = container_of(fp->private_data, struct ip_component_dev, miscdev);
+    loff_t max_offset = RAM_SPAN;
+    loff_t next_offset;
+    pr_info("  dev_llseek: enter\n");
+    pr_info("   dev_llseek: offset = %lld\n", offset);
+    pr_info("   dev_llseek: whence = %d\n", whence);
+    switch(whence)
+    {
+        case SEEK_SET:
+            next_offset = offset;
+            break;
+        case SEEK_CUR:
+            next_offset = fp->f_pos + offset;
+            break;
+        case SEEK_END:
+            next_offset = max_offset;
+            break;
+        default:
+            pr_info("   dev_llseek: bad whence exit\n");
+            return -EINVAL;
+    }
+    if (next_offset < 0)
+    {
+        pr_info("   dev_llseek: negative offset exit\n");
+        return -EINVAL;
+    }
+    if (next_offset > max_offset)
+        next_offset = max_offset;
+    fp->f_pos = next_offset;
+    pr_info("  dev_llseek: exit\n");
+    return next_offset;
+}
+// Called when writing to the device.
+static ssize_t dev_write(struct file *fp, const char __user *user_buffer, size_t len, loff_t *offset)
 {
     int success = 0;
-    /* 
-    * Get the avalon_rpm_dev structure out of the miscdevice structure.
-    * Remember, the Misc subsystem has a default "open" function that will set
-    * "file"s private data to the appropriate miscdevice structure. We then use the
-    * container_of macro to get the structure that miscdevice is stored inside of (which
-    * is our avalon_rpm_dev structure that has the current rpm value).
-    */
-    struct avalon_rpm_dev *dev = container_of(file->private_data, struct avalon_rpm_dev, miscdev);
-    pr_info(" rpm_write: enter\n");
-    // Get the new rpm value (this is just the first byte of the given data)
-    success = copy_from_user(&dev->rpm_value, buffer, sizeof(dev->rpm_value));
-    // If we failed to copy the value from userspace, display an error message
+    struct ip_component_dev *dev = container_of(fp->private_data, struct ip_component_dev, miscdev);
+    pr_info("  dev_write: len = %d\n", len);
+    pr_info("  dev_write: offset = %lld\n", *offset);
+    // Get ip_component_dev structure out of the miscdevice structure.
+    success = copy_from_user(&dev->io_buf, user_buffer, sizeof(dev->io_buf));
     if(success != 0)
     {
-        pr_info(" rpm_write: Failed to read rpm value from userspace\n");
-        return -EFAULT; // Bad address error value. It's likely that "buffer" doesn't point to a good address
+        pr_info("  dev_write: Failed to read value from userspace.\n");
+        return -EFAULT;
     }
     else
     {
-        // We read the data correctly, so update the PWM
-        iowrite32(dev->rpm_value, dev->regs);
+        iowrite32(dev->io_buf[*offset], dev->regs);
     }
-    // Tell the user process that we wrote every byte they sent 
-    // (even if we only wrote the first value, this will ensure they don't try to re-write their data)
-    pr_info(" rpm_write: exit\n");
     return len;
 }
-static int dev_release(struct inode *inodep, struct file *filep)
+// Called when the device is released.
+static int dev_release(struct inode *ip, struct file *fp)
 {
-   printk(KERN_INFO " dev_release: Device successfully closed.\n");
-   return 0;
-}
-// Gets called whenever a device this driver handles is removed.
-// This will also get called for each device being handled when 
-// our driver gets removed from the system (using the rmmod command).
-static int rpm_remove(struct platform_device *pdev)
-{
-    // Grab the instance-specific information out of the platform device
-    struct avalon_rpm_dev *dev = (struct avalon_rpm_dev*)platform_get_drvdata(pdev);
-    pr_info(" rpm_remove: enter\n");
-    // Turn PWM off
-    iowrite32(0x00, dev->regs);
-    // Unregister the character file (remove it from /dev)
-    misc_deregister(&dev->miscdev);
-    pr_info(" rpm_remove: exit\n");
+    pr_info(" dev_release: %s\n", "enter.");
+    pr_info(" dev_open: %s\n", "--- DEVICE RELEASE ---");
     return 0;
 }
-// Called when the driver is removed
-static void rpm_exit(void)
+// Gets called whenever a device this driver handles is removed.
+static int dev_remove(struct platform_device *pdev)
 {
-    pr_info(" rpm_exit: Avalon PWM module exit\n");
+    // Grab the instance-specific information out of the platform device
+    struct ip_component_dev *dev = (struct ip_component_dev*)platform_get_drvdata(pdev);
+    // pr_info(" dev_remove: enter\n");
+    pr_info(" dev_remove: Unregistering %s device...\n", dev->miscdev.name);
+    misc_deregister(&dev->miscdev);
+    return 0;
+}
+// Called when the driver is removed.
+static void dev_exit(void)
+{
+    pr_info("dev_exit: Unregistering %s.\n", name);
     // Unregister our driver from the "Platform Driver" bus
-    // This will cause "rpm_remove" to be called for each connected device
-    platform_driver_unregister(&rpm_platform);
-    pr_info(" rpm_exit: Avalon PWM module successfully unregistered\n");
-    pr_info(" ***\n");
-    pr_info(" \n");
+    platform_driver_unregister(&driver_platform);
+    pr_info("dev_exit: %s successfully unregistered\n\n", name);
 }
 // Tell the kernel which functions are the initialization and exit functions
-module_init(rpm_init);
-module_exit(rpm_exit);
-// Define information about this kernel module
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Franz Luepke <fk.luepke146@uniandes.edu.co>");
-MODULE_DESCRIPTION("Exposes a character device to user space that lets users change PWM values");
-MODULE_VERSION("1.0");
+module_init(dev_init);
+module_exit(dev_exit);
--- software/userspace/avalon_rpm/devmem_avalon_rpm.c
+++ software/userspace/avalon_rpm/devmem_avalon_rpm.c
@@ -14,19 +14,19 @@ int main(int argc, char ** argv)
 {
 	void * lw_bridge_map = 0;
 	uint32_t * rpm_0_map = 0;
-	uint32_t * rpm_1_map = 0;
-	uint32_t * rpm_2_map = 0;
-	uint32_t * rpm_3_map = 0;
-	uint32_t * rpm_4_map = 0;
-	uint32_t * rpm_5_map = 0;
+	// uint32_t * rpm_1_map = 0;
+	// uint32_t * rpm_2_map = 0;
+	// uint32_t * rpm_3_map = 0;
+	// uint32_t * rpm_4_map = 0;
+	// uint32_t * rpm_5_map = 0;
 	int devmem_fd = 0;
 	int result = 0;
 	int rpm_0_value = 0;
-	int rpm_1_value = 0;
-	int rpm_2_value = 0;
-	int rpm_3_value = 0;
-	int rpm_4_value = 0;
-	int rpm_5_value = 0;
+	// int rpm_1_value = 0;
+	// int rpm_2_value = 0;
+	// int rpm_3_value = 0;
+	// int rpm_4_value = 0;
+	// int rpm_5_value = 0;
 	// Open up the /dev/mem device (aka, RAM)
 	devmem_fd = open("/dev/mem", O_RDWR | O_SYNC);
 	if(devmem_fd < 0)
@@ -44,24 +44,37 @@ int main(int argc, char ** argv)
 	}
 	// Set the rpm_map to the correct offset within the RAM (AVALON_RPM_X_BASE is from "hps_0.h")
 	rpm_0_map = (uint32_t*)(lw_bridge_map + AVALON_RPM_0_BASE);
-	rpm_1_map = (uint32_t*)(lw_bridge_map + AVALON_RPM_1_BASE);
-	rpm_2_map = (uint32_t*)(lw_bridge_map + AVALON_RPM_2_BASE);
-	rpm_3_map = (uint32_t*)(lw_bridge_map + AVALON_RPM_3_BASE);
-	rpm_4_map = (uint32_t*)(lw_bridge_map + AVALON_RPM_4_BASE);
-	rpm_5_map = (uint32_t*)(lw_bridge_map + AVALON_RPM_5_BASE);
+	// rpm_1_map = (uint32_t*)(lw_bridge_map + AVALON_RPM_1_BASE);
+	// rpm_2_map = (uint32_t*)(lw_bridge_map + AVALON_RPM_2_BASE);
+	// rpm_3_map = (uint32_t*)(lw_bridge_map + AVALON_RPM_3_BASE);
+	// rpm_4_map = (uint32_t*)(lw_bridge_map + AVALON_RPM_4_BASE);
+	// rpm_5_map = (uint32_t*)(lw_bridge_map + AVALON_RPM_5_BASE);
 	printf(" Reading all RPM outputs...\n");
 	while(1)
 	{
-		rpm_0_value = (int) rpm_0_map[0];
-		rpm_1_value = (int) rpm_1_map[0];
-		rpm_2_value = (int) rpm_2_map[0];
-		rpm_3_value = (int) rpm_3_map[0];
-		rpm_4_value = (int) rpm_4_map[0];
-		rpm_5_value = (int) rpm_5_map[0];
-		printf("\r \33[2K Value read: [%x] [%x] [%x] [%x] [%x] [%x]", rpm_0_value, rpm_1_value, rpm_2_value, rpm_3_value, rpm_4_value, rpm_5_value);
-		fflush(stdout);
+		printf(" Value read:");
+		for (int i = 0; i < 40; ++i)
+		{
+			rpm_0_value = (int) rpm_0_map[i];
+			printf(" [%d]", rpm_0_value);
+		}
+		printf("\n");
+		// rpm_0_value = (int) rpm_0_map[0];
+		// rpm_1_value = (int) rpm_0_map[4];
+		// rpm_2_value = (int) rpm_0_map[8];
+		// rpm_3_value = (int) rpm_0_map[12];
+		// rpm_5_value = (int) rpm_0_map[16];
+
+		// rpm_1_value = (int) rpm_1_map[0];
+		// rpm_2_value = (int) rpm_2_map[0];
+		// rpm_3_value = (int) rpm_3_map[0];
+		// rpm_4_value = (int) rpm_4_map[0];
+		// rpm_5_value = (int) rpm_5_map[0];
+		// printf("  Value read: [%x] [%x] [%x] [%x] [%x] [%x]\n", rpm_0_value, rpm_1_value, rpm_2_value, rpm_3_value, rpm_4_value, rpm_5_value);
+		// printf("\r \33[2K Value read: [%x] [%x] [%x] [%x] [%x] [%x]", rpm_0_value, rpm_1_value, rpm_2_value, rpm_3_value, rpm_4_value, rpm_5_value);
+		// fflush(stdout);
 		// Wait half a second
-		usleep(1000);
+		usleep(1000000);
 	}
 	printf("Done!\n");
 	// Unmap everything and close the /dev/mem file descriptor
